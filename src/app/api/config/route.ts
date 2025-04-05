32d30dd18c898ebcee6b5e27a7b8cbd8562d2345
sjiampojamarn <18257803+sjiampojamarn@users.noreply.github.com>
2025-04-05 15:09:35 -0700

    Disable getting api keys from setting page

--- src/app/api/config/route.ts
+++ src/app/api/config/route.ts
@@ -49,15 +49,16 @@ export const GET = async (req: Request) => {
       });
     }
 
-    config['openaiApiKey'] = getOpenaiApiKey();
-    config['ollamaApiUrl'] = getOllamaApiEndpoint();
-    config['anthropicApiKey'] = getAnthropicApiKey();
-    config['groqApiKey'] = getGroqApiKey();
-    config['geminiApiKey'] = getGeminiApiKey();
-    config['deepseekApiKey'] = getDeepseekApiKey();
-    config['customOpenaiApiUrl'] = getCustomOpenaiApiUrl();
-    config['customOpenaiApiKey'] = getCustomOpenaiApiKey();
-    config['customOpenaiModelName'] = getCustomOpenaiModelName();
+    // SJ: Disable getting api keys
+    // config['openaiApiKey'] = getOpenaiApiKey();
+    // config['ollamaApiUrl'] = getOllamaApiEndpoint();
+    // config['anthropicApiKey'] = getAnthropicApiKey();
+    // config['groqApiKey'] = getGroqApiKey();
+    // config['geminiApiKey'] = getGeminiApiKey();
+    // config['deepseekApiKey'] = getDeepseekApiKey();
+    // config['customOpenaiApiUrl'] = getCustomOpenaiApiUrl();
+    // config['customOpenaiApiKey'] = getCustomOpenaiApiKey();
+    // config['customOpenaiModelName'] = getCustomOpenaiModelName();
 
     return Response.json({ ...config }, { status: 200 });
   } catch (err) {
@@ -101,7 +102,8 @@ export const POST = async (req: Request) => {
       },
     };
 
-    updateConfig(updatedConfig);
+  // Disable updating api keys 
+  // updateConfig(updatedConfig);
 
     return Response.json({ message: 'Config updated' }, { status: 200 });
   } catch (err) {
