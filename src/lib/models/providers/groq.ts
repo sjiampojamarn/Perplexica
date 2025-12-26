0a9563aa6a49939f95bb57d21336106e560bf0e8
sjiampojamarn <18257803+sjiampojamarn@users.noreply.github.com>
2025-12-25 18:48:21 -0800

    add qroq default models

--- src/lib/models/providers/groq.ts
+++ src/lib/models/providers/groq.ts
@@ -40,12 +40,32 @@ class GroqProvider extends BaseModelProvider<GroqConfig> {
 
       const data = await res.json();
 
-      const models: Model[] = data.data.map((m: any) => {
+      // SJ: default models for free tier friendly models
+      let models: Model[] = [
+        {
+          name: 'meta-llama/llama-4-scout-17b-16e-instruct',
+          key: 'meta-llama/llama-4-scout-17b-16e-instruct'
+        },
+        {
+          name: 'llama-3.3-70b-versatile',
+          key: 'llama-3.3-70b-versatile'
+        },
+        {
+          name: 'moonshotai/kimi-k2-instruct',
+          key: 'moonshotai/kimi-k2-instruct'
+        },
+        {
+          name: 'moonshotai/kimi-k2-instruct-0905',
+          key: 'moonshotai/kimi-k2-instruct-0905'
+        }
+      ];
+
+      models.push(...data.data.map((m: any) => {
         return {
           name: m.id,
           key: m.id,
         };
-      });
+      }));
 
       return {
         embedding: [],
